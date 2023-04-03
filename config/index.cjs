4ca0f8f9a7c96a48649edf806ec3fa77703cf25f
Xiao-Gui123 <107522134+Xiao-Gui123@users.noreply.github.com>
2023-04-03 21:23:17 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -69,6 +69,36 @@ const USER_CONFIG = {
         { keyword: 'love_day', date: '2023-01-25' },
       ],
     },
+     {
+      // 想要发送的人的名字
+      name: '泽宇',
+      // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
+      id: 'o71lh6ZX7v1vAviBJxbEcH0a53Aw',
+      // 使用微信测试号：你想对他发送的模板消息的模板ID
+      useTemplateId: '3TRS-A9n01as_pw5bSzK6uEpyJm9195w8_cOhcXHgt8',
+      // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
+      horoscopeDate: '12-27',
+      festivals: [
+        // 注意：此条配置日期为阴历日期，因为`type`中 “生日” 之前有 * 符号
+        {
+          type: '*生日', name: '宝贝', year: '1996', date: '09-09',
+        },
+        // 注意：此条配置日期为阳历日期，因为`type`中 “生日” 之前没有 * 符号
+        {
+          type: '生日', name: '李四', year: '1996', date: '09-31',
+        },
+        {
+          type: '节日', name: '相识纪念日', year: '2020', date: '09-03',
+        },
+      ],
+      // 我们在一起已经有xxxx天了的配置
+      customizedDateList: [
+        // 在一起的日子
+        { keyword: 'love_day', date: '2022-09-08' },
+        // 结婚纪念日
+        { keyword: 'marry_day', date: '2022-09-09' },
+      ],
+    },
   ],
 
 

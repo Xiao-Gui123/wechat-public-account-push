920db1755538a7965d616b75bfb9d7b7575c6029
Xiao-Gui123 <107522134+Xiao-Gui123@users.noreply.github.com>
2023-04-03 21:15:59 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -58,16 +58,10 @@ const USER_CONFIG = {
       horoscopeDate: '01-12',
       festivals: [
         // 注意：此条配置日期为阴历日期，因为`type`中 “生日” 之前有 * 符号
-        {
-          type: '*生日', name: '宝贝', year: '1996', date: '09-09',
-        },
         // 注意：此条配置日期为阳历日期，因为`type`中 “生日” 之前没有 * 符号
         {
-          type: '生日', name: '李四', year: '1996', date: '09-31',
-        },
-        {
-          type: '节日', name: '相识纪念日', year: '2020', date: '09-03',
-        },
+          type: '生日', name: '宝贝', year: '2000', date: '11-25',
+        }
       ],
       // 我们在一起已经有xxxx天了的配置
       customizedDateList: [
